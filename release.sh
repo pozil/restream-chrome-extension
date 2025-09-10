2c5922312fda90b7b76b4aef67c90cf17c1febc8
Philippe Ozil <pozil@salesforce.com>
2025-09-10 13:32:06 +0200

    build: improved release script

--- release.sh
+++ release.sh
@@ -1,2 +1,11 @@
-rm ~/Desktop/restream-chrome-extension.zip
-zip -r ~/Desktop/restream-chrome-extension.zip . --exclude release.sh .git/\* \*.DS_Store
+#!/bin/bash
+
+BUILD_FILE_PATH=~/Desktop/restream-chrome-extension.zip
+
+# Remove existing zip file if present
+if [ -f $BUILD_FILE_PATH ];  then
+  rm $BUILD_FILE_PATH
+fi
+
+# Create build
+zip -r $BUILD_FILE_PATH . --exclude release.sh .git/\* \*.DS_Store
